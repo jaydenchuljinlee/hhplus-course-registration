dada30598d179b1001900dace4e8c3b8dcc807e0
cheoljin <cheoljin1234@naver.com>
2024-09-28 22:03:42 +0900

    init project

---
+++ src/main/kotlin/com/hhplus/enrollment/EnrollmentApplication.kt
@@ -0,0 +1,11 @@
+package com.hhplus.enrollment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnrollmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnrollmentApplication>(*args)
+}
