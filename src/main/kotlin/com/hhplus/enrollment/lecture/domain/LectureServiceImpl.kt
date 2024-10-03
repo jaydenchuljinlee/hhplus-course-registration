cb582e3a7d684a00ae436bb6b802ed0c0be6321f
이철진 <ironjin92@joonggonara.co.kr>
2024-10-04 01:13:54 +0900

    Refactor: 강이 이력 중복 저장 이슈 처리

--- src/main/kotlin/com/hhplus/enrollment/lecture/domain/LectureServiceImpl.kt
+++ src/main/kotlin/com/hhplus/enrollment/lecture/domain/LectureServiceImpl.kt
@@ -28,9 +28,10 @@ class LectureServiceImpl(
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     override fun enroll(query: LectureCommandData): LectureData {
+        val lecture = lectureRepository.getLecture(LectureQueryDto(query.lectureId)) // 강의 정보 조회. 내부에서 유효성 검사
+
         validator.validate(query) // 수강자 정보, 신청 이력에 대한 유효성 검사
 
-        val lecture = lectureRepository.getLecture(LectureQueryDto(query.lectureId)) // 강의 정보 조회. 내부에서 유효성 검사
         lecture.enroll() // 수강 신청. 내부에서 유효성 검사
 
         lectureRepository.enroll(lecture) // 참감 된 수강 정보 저장
